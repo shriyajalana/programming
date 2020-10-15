ac10a1f427f27940a92f94ba454015c02b4a29fa
Afroz2021 <72924387+Afroz2021@users.noreply.github.com>
2020-10-15 18:01:00 +0530

    Update stlvector.cpp

--- stlvector.cpp
+++ stlvector.cpp
@@ -1,11 +1,12 @@
+// This is the thing where you update everything regarding vectore
 #include<iostream>
-#include<vector>
+#include<vector>  // This is the headerfile of vector
 using namespace std;
 int main()
 { int n;
 cout<<"Enter Array Size \n";
 cin>>n;
-vector<int> a(n);
+vector<int> a(n);    // creating a a vector
 
 int i,sum=0;
 for(int i=0;i<a.size();i++)
