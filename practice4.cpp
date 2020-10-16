58734bde21d33a6e81a33037b6b4c041c1db095c
98afroz <72955961+98afroz@users.noreply.github.com>
2020-10-16 07:56:26 +0530

    Update practice4.cpp

--- practice4.cpp
+++ practice4.cpp
@@ -1,9 +1,13 @@
+//Swaping two Number without introducing 3rd Variable
 #include<iostream>
-#include<vector>
 using namespace std;
 int main()
 {
-	vector<int> a;
-	cin>>a;
-	cout<<a;
+	int a,b;
+	cin>>a>>b;  //if a=3,b=4
+	a=a+b;     //a=7 b=4
+	b=a-b;     // a=7 b=3
+	a=a-b;     //a=4 b=3
+	cout<<"Swaped:  "<<a<<b;  //Successfully swaped both number
+	return 0;
 }
