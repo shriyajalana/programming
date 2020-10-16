d833d2925962374d00963c1c6671b0d8ccda3ac6
98afroz <72955961+98afroz@users.noreply.github.com>
2020-10-16 07:58:02 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,15 +1,26 @@
-//Calculating factorial of a Entered number
+//Calculating factorial of a Entered number using recursion
 
 #include<iostream>
 using namespace std;
+
+int factorial(int n);
+
 int main()
 {
-    int n,i,fact=1;
+    int n;
+
+    cout << "Enter a positive integer: ";
     cin >> n;
-    for(i=1;i<=n;i++)
-    {
-        fact*=i;
-    }
-    cout<<fact;
-    return 0;    
+
+    cout << "Factorial of " << n << " = " << factorial(n);
+
+    return 0;
+}
+
+int factorial(int n)
+{
+    if(n > 1)
+        return n * factorial(n - 1);
+    else
+        return 1;
 }
