1158926d7ab71ec738b17d45c6d047d11f044868
TechyAfroz <72185888+TechyAfroz@users.noreply.github.com>
2021-10-07 15:24:56 +0530

    Update summation.cpp

--- summation.cpp
+++ summation.cpp
@@ -1,27 +1,13 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
-    long long int n;
+    int n;
     cin >> n;
-    long long int a[10] = {0};
-    while (n != 0)
-    {
-        long long int k = n % 10;
-        a[k]++;
-        n = n / 10;
-    }
-    for (long long int i = 0; i < 10; i++)
-    {
-        if (a[i] == 0)
-        {
-            continue;
-        }
-        else
-        {
-            cout << i << " :" << a[i] << "\n";
-        }
-    }
-    cout << endl;
+    string str;
+    str = to_string(n);
+    int k = str.length();
+    cout << str[0] << "   " << str << "   " << k << endl;
     return 0;
 }
