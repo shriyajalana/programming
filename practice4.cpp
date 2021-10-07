6f054e7eabfc5b25122c92f5fe1f82108db19332
98afroz <72955961+98afroz@users.noreply.github.com>
2021-10-07 10:57:17 +0530

    Update practice4.cpp

--- practice4.cpp
+++ practice4.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int main()
 {
 	int a,b;
-	cin>>a>>b;  //if a=3,b=4
-	a=a+b;     //a=7 b=4
-	b=a-b;     // a=7 b=3
-	a=a-b;     //a=4 b=3
-	cout<<"Swaped:  "<<a<<b;  //Successfully swaped both number
+	cin>>a>>b;
+	int c;
+	c=a;
+	a=b;
+	b=c;
+	cout<<"Swaped number is: "<<a<<b<<endl;
 	return 0;
 }
