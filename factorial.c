98bb4db4e1867d34bce0d075fc6b4cdc96f8328b
TechyAfroz <72185888+TechyAfroz@users.noreply.github.com>
2021-10-07 15:24:27 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,28 +1,9 @@
-//finding factorial of given number
-#include<stdio.h>
+#include <iostream>
+using namespace std;
 int main()
 {
-    int a[200],n,counter,temp,i;
-    a[0]=1;
-    counter=0;
-    printf("Enter the number to Find Factorial: ");
-    scanf("%d",&n);
-    for(; n>=2; n--)
-    {
-        temp=0;
-        for(i=0; i<=counter; i++)
-        {
-            temp=(a[i]*n)+temp;
-            a[i]=temp%10;
-            temp=temp/10;
-        }
-        while(temp>0)
-        {
-            a[++counter]=temp%10;
-            temp=temp/10;
-        }
-    }
-    for(i=counter; i>=0; i--)
-        printf("%d",a[i]);
+    char c;
+    c = 100;
+    cout << char(c) << endl;
     return 0;
 }
