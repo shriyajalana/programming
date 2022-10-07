064c7eccc0bd4408617e507a484d6fca29fe21a8
naziya135 <73421205+naziya135@users.noreply.github.com>
2022-10-07 22:01:40 +0530

    Update practice2.cpp

--- practice2.cpp
+++ practice2.cpp
@@ -1,66 +1,33 @@
-// Delete a node at nth position
+// 1,2,3,6,4,5,7,6,9
 
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-struct Node{
-    int data;
-    Node* next;
-};
-Node* head;
-
-void Insert(int data){
-    Node* temp=new Node;
-    temp->data=data;
-    temp->next=NULL;
-    if(head==NULL){
-        head=temp;
-        return;
+int main()
+{
+    int n;
+    cin >> n;
+    int array[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> array[i];
     }
-    Node* temp1=head;
-    while(temp1->next!=NULL){
-        temp1=temp1->next;
+    vector<int> myVect;
+    int k;
+    cin >> k;
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i] != k)
+        {
+            myVect.push_back(array[i]);
+        }
     }
-    temp1->next=temp;   
-    
+    cout << "Vector is: ";
+    for (int j = 0; j < myVect.size(); j++)
+    {
+        cout << myVect[j] << ", ";
+    }
+    cout << endl;
 }
 
-void Print(){
-    Node* temp=head;
-    while(temp!=NULL){
-        cout<<temp->data<<"  ";
-        temp=temp->next;
-    }
-    cout<<"\n";
-}
-
-void Delete(int p){
-    Node* temp;
-    temp=head;
-    if(p==1){
-        head=temp->next;
-        free(temp);     //delete node
-        return;
-    }
-    Node* temp1=head;
-    for(int i=0;i<p-2;i++){
-        temp1=temp1->next;   //p-1 node
-    }
-    Node* temp2=temp1->next; //p node
-    temp1->next=temp2->next; // connection p-1th node to p+1th node 
-    free(temp2);   //delete node free pth node
-}
-
-int main(){
-    head=NULL;
-    Insert(7);
-    Insert(9);
-    Insert(2);
-    Insert(45);
-    Print();
-    cout<<"Enter position to delete \n";
-    int pos;
-    cin>>pos;
-    Delete(pos);
-    Print();
-    return 0;
-}
+// 1,2,3,4,5
