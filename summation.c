a0b03a3227ebba08520a620ac04d48588dec61f6
Afroz2021 <72924387+Afroz2021@users.noreply.github.com>
2022-10-07 21:43:46 +0530

    Update summation.c

--- summation.c
+++ summation.c
@@ -1,22 +1,49 @@
-
+// insert a node at the beginning of the list
 #include <iostream>
 using namespace std;
+
+struct Node
+{
+    int data; //creating a node with two variable one is data and another one is pointer
+    Node *next;
+};
+
+Node *Insert(Node *head, int x)
+{
+    Node *temp = new Node; //creating a node
+    temp->data = x;        //storing data in the node
+    temp->next = NULL;     //storing address in the node
+
+    temp->next = head; // linking node with other node         phle jodo
+    head = temp;       // breaking a link and adding with the head   phir todo
+    return head;
+}
+
+void Print(Node *temp)
+{
+    cout << "Here is the list: ";
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+        cout << temp->data << " -> ";
+    }
+    cout << endl;
+    cout << temp->data << "  ";
+    cout << "\n";
+}
+
 int main()
 {
-    int n, i, j;
+    Node *head = NULL;
+    int i, n, x;
+    cout << "How many number?\n";
     cin >> n;
-    for (j = 2; j <= n; j++)
+    for (i = 0; i < n; i++)
     {
-        for (i = 2; i < j; i++)
-        {
-            if (j % i == 0)
-            {
-                break;
-            }
-        }
-        if (i == j)
-        {
-            cout << j << endl;
-        }
+        cout << "Enter Number\n";
+        cin >> x;
+        head = Insert(head, x);
     }
+    Print(head);
+    return 0;
 }
