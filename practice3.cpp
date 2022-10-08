4b7476a3dbe8a7f026a674bd4cfe45265b6d0d84
TechyAfroz <72185888+TechyAfroz@users.noreply.github.com>
2022-10-08 12:25:08 +0530

    Update practice3.cpp

--- practice3.cpp
+++ practice3.cpp
@@ -1,47 +1,21 @@
-//printing number in farward and backward direction using recursion method
-
 #include<iostream>
+#include<string.h>
 using namespace std;
-struct Node{
-    int data;
-    Node* next;
-};
 
-Node* Insert(Node* head,int n){
-    Node* temp=new Node;
-    temp->data=n;
-    temp->next=NULL;
-    if(head==NULL){
-        head=temp;
-        return;
+void print(char* c)            // c[i]==*(c+i)   &&    &c[i]==(c+i)              
+{
+    c[0]='k';
+    while(*c!='\0')
+    {
+        cout<<*c;
+        c++;
     }
-    else{
-        Node* temp1;
-        temp1=head;
-        while(temp1->next!=NULL){
-            temp1=temp1->next;
-        }
-        temp1->next=temp;
-    }
-    return head;
+    cout<<"\n";
 }
 
-void Print(Node* head){
-    if(head==NULL){
-        cout<<"\n";
-        return;
-    }
-    cout<<head->data<<"  ";      // Number will print in the forward direction
-    Print(head->next);           //recursive call of Print function
-    cout<<head->data<<"  ";      //Number will print in the backward direction
-}
-
-int main(){
-    Node* head=NULL;
-    head=Insert(head,2);
-    head=Insert(head,5);
-    head=Insert(head,7);
-    head=Insert(head,4);
-    Print(head);
-    return 0;
+int main()
+{
+    //char c[6]= {'a','f','r','o','z','\0'};
+    char c[68]= "Afroziiitian";
+    print(c);
 }
