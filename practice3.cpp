3c5716f8846823bd87079eb534886e061230b3b5
Sahwan Khan <56104387+shahwan786@users.noreply.github.com>
2022-10-08 12:52:47 +0530

    Update practice3.cpp

--- practice3.cpp
+++ practice3.cpp
@@ -1,21 +1,33 @@
-#include<iostream>
-#include<string.h>
+// 1,2,3,6,4,5,7,6,9
+
+#include <iostream>
+#include <vector>
 using namespace std;
-
-void print(char* c)            // c[i]==*(c+i)   &&    &c[i]==(c+i)              
-{
-    c[0]='k';
-    while(*c!='\0')
-    {
-        cout<<*c;
-        c++;
-    }
-    cout<<"\n";
-}
-
 int main()
 {
-    //char c[6]= {'a','f','r','o','z','\0'};
-    char c[68]= "Afroziiitian";
-    print(c);
+    int n;
+    cin >> n;
+    int array[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> array[i];
+    }
+    vector<int> myVect;
+    int k;
+    cin >> k;
+    for (int i = 0; i < n; i++)
+    {
+        if (array[i] != k)
+        {
+            myVect.push_back(array[i]);
+        }
+    }
+    cout << "Vector is: ";
+    for (int j = 0; j < myVect.size(); j++)
+    {
+        cout << myVect[j] << ", ";
+    }
+    cout << endl;
 }
+
+// 1,2,3,4,5
