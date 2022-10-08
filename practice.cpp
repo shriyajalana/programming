645bb076b0fc6ed956cad885ddb463d0cae05826
TechyAfroz <72185888+TechyAfroz@users.noreply.github.com>
2022-10-08 12:24:17 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,66 +1,51 @@
-// Delete a node at nth position
-
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-struct Node{
-    int data;
-    Node* next;
-};
-Node* head;
 
-void Insert(int data){
-    Node* temp=new Node;
-    temp->data=data;
-    temp->next=NULL;
-    if(head==NULL){
-        head=temp;
-        return;
+vector<vector<int>> help(vector<int> &nums,vector<vector<int>> &v,vector<int> temp, int k,int idx){
+  if(k<0){
+    return v;
+  }
+  if(idx>=nums.size()){
+    if(k==0){
+      v.push_back(temp);
     }
-    Node* temp1=head;
-    while(temp1->next!=NULL){
-        temp1=temp1->next;
-    }
-    temp1->next=temp;   
-    
+    return v;
+  }
+
+  temp.push_back(nums[idx]);
+  help(nums,v,temp,k-nums[idx],idx+1);
+  temp.pop_back();
+  help(nums,v,temp,k,idx+1);
+  return v;
 }
 
-void Print(){
-    Node* temp=head;
-    while(temp!=NULL){
-        cout<<temp->data<<"  ";
-        temp=temp->next;
-    }
-    cout<<"\n";
+vector<vector<int>> solve(vector<int> &nums,int k){
+  vector<vector<int>> v;
+  vector<int> temp;
+  return help(nums,v,temp,k,0);
 }
 
-void Delete(int p){
-    Node* temp;
-    temp=head;
-    if(p==1){
-        head=temp->next;
-        free(temp);     //delete node
-        return;
-    }
-    Node* temp1=head;
-    for(int i=0;i<p-2;i++){
-        temp1=temp1->next;   //p-1 node
-    }
-    Node* temp2=temp1->next; //p node
-    temp1->next=temp2->next; // connection p-1th node to p+1th node 
-    free(temp2);   //delete node free pth node
-}
 
 int main(){
-    head=NULL;
-    Insert(7);
-    Insert(9);
-    Insert(2);
-    Insert(45);
-    Print();
-    cout<<"Enter position to delete \n";
-    int pos;
-    cin>>pos;
-    Delete(pos);
-    Print();
-    return 0;
+  #ifndef ONLINE_JUDGE
+    freopen("Error.txt", "w", stderr); 
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+  int n,k;
+  cin>>n>>k;
+  vector<int> v;
+  for(int i=0;i<n;i++){
+    int val;
+    cin>>val;
+    v.push_back(val);
+  }
+  vector<vector<int>> ans=solve(v,k);
+  for(int i=0;i<ans.size();i++){
+    for(auto it: ans[i]){
+      cout<<it<<" ";
+    }
+    cout<<endl;
+  }
+  return 0;
 }
