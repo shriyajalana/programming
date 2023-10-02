0ed12c05cf00d3033e2a08659a8793459fe54d42
afrozxman <72957058+afrozxman@users.noreply.github.com>
2023-10-02 15:29:11 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,57 +1,11 @@
-#include<iostream>
-using namespace std;
-int main(){
-    string n;
-    int i;
-    cin>>n;
-    int a=0,b=0,c=0,d=0,e=0,f=0,g=0,h=0,k=0,l=0;
-    for(i=0;i<n.length();i++){
-        
-        
-        if(n[i]=='0'){
-            a++;
-        }
-        if(n[i]=='1'){
-            b++;
-        }
-        if(n[i]=='2'){
-            c++;
-        }
-        if(n[i]=='3'){
-            d++;
-        }
-         if(n[i]=='4'){
-            e++;
-        }
-         if(n[i]=='5'){
-            f++;
-        }
-         if(n[i]=='6'){
-            g++;
-        }
-         if(n[i]=='7'){
-            h++;
-        }
-         if(n[i]=='8'){
-            k++;
-        }
-         if(n[i]=='9'){
-            l++;
-        }
-    }
-    cout<<"0"<<" "<<a<<"\n";
-    cout<<"1"<<" "<<b<<"\n";
-    cout<<"2"<<" "<<c<<"\n";
-    cout<<"3"<<" "<<d<<"\n";
-    cout<<"4"<<" "<<e<<"\n";
-    cout<<"5"<<" "<<f<<"\n";
-    cout<<"6"<<" "<<g<<"\n";
-    cout<<"7"<<" "<<h<<"\n";
-    cout<<"8"<<" "<<k<<"\n";
-    cout<<"9"<<" "<<l<<"\n";
-    
-    
+import React from 'react';
+import {Link} from 'react-router-dom'
+import './BackToEvents.css'
 
-
-    return 0;
+export const BackToEvents = () => {
+	return (
+		<div className="events_back">
+				<Link to="/events"><span className="fas fa-arrow-left"></span></Link>
+		</div>
+	);
 }
