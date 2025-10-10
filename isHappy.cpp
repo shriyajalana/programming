274d70669ea47e1d5f9dd62e7e389c309783a430
naziya135 <73421205+naziya135@users.noreply.github.com>
2025-10-10 17:31:45 +0530

    Create isHappy.cpp

---
+++ isHappy.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    int squareDigit(int n){
+        int sum=0;
+        while(n){
+            int x=n%10;
+            sum=sum+(x*x);
+            n=n/10;
+        }
+        return sum;
+    }
+    bool isHappy(int n) {
+        map<int, int> mp;
+        while(!(mp.find(n)!=mp.end())){
+            mp[n]=1;
+            n=squareDigit(n);
+            if(n==1) return true;
+        }
+        return false;
+    }
+};
