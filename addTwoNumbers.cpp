fa942503d6b170989dee412f2655c1cc2144db1a
afroz2020 <72923119+afroz2020@users.noreply.github.com>
2025-10-10 16:56:41 +0530

    Create addTwoNumbers.cpp

---
+++ addTwoNumbers.cpp
@@ -0,0 +1,20 @@
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    ListNode dummy(0);
+    ListNode* cur = &dummy;
+    int carry = 0;
+    while (l1 || l2 || carry) {
+        int sum = carry + (l1 ? l1->val : 0) + (l2 ? l2->val : 0);
+        carry = sum / 10;
+        cur->next = new ListNode(sum % 10);
+        cur = cur->next;
+        if (l1) l1 = l1->next;
+        if (l2) l2 = l2->next;
+    }
+    return dummy.next;
+}
